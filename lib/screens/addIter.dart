185d397a785cfac33489f974af01aa6fb92642ab
Wesley Nogueira <wesleynogueiraps@hotmail.com>
2026-07-13 15:16:53 -0300

    feat: Finalizando desenho do formulário

--- lib/screens/addIter.dart
+++ lib/screens/addIter.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:iter/Utils/currencyFormat.dart';
+import 'package:iter/Utils/bairros.dart';
 
 class AddIter extends StatefulWidget {
   const AddIter({super.key});
@@ -10,10 +11,17 @@ class AddIter extends StatefulWidget {
 }
 
 class _AddIterState extends State<AddIter> {
+  List<String> bairros = fortaleza_bairros;
   int selectedCompanyIndex = 0;
   String? status;
   DateTime selectedDate = DateTime.now();
+  String? selectedBairro;
+
   TextEditingController valueController = TextEditingController();
+  TextEditingController kmInicialController = TextEditingController();
+  TextEditingController kmFinalController = TextEditingController();
+  TextEditingController pctInicialController = TextEditingController();
+  TextEditingController pctFinalController = TextEditingController();
 
   void _showCupertinoDatePicker(BuildContext context) {
     showCupertinoModalPopup<void>(
@@ -260,6 +268,12 @@ class _AddIterState extends State<AddIter> {
                       borderRadius: BorderRadius.circular(10),
                     ),
                   ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Por favor, insira um valor';
+                    }
+                    return null;
+                  },
                 ),
                 SizedBox(height: 30),
                 const Text(
@@ -267,6 +281,153 @@ class _AddIterState extends State<AddIter> {
                   style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
                 ),
                 const Divider(color: Colors.grey, thickness: 1.0),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text('KM'),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: TextField(
+                        controller: kmInicialController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          hintText: 'Inicial',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(' - '),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: TextFormField(
+                        controller: kmFinalController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          hintText: 'Final',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: (value) {
+                          if ((value == null || value.isEmpty) &&
+                              kmInicialController.text.isNotEmpty) {
+                            return 'Por favor, insira o KM final';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text('Rota (Pacote | Parada)'),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: TextField(
+                        controller: pctInicialController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          hintText: 'PCT',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(' - '),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: TextFormField(
+                        controller: pctFinalController,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          hintText: 'Parada',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: (value) {
+                          if ((value == null || value.isEmpty) &&
+                              pctInicialController.text.isNotEmpty) {
+                            return 'Por favor, insira o PCT final';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text("Bairro"),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: DropdownButtonFormField<String>(
+                        decoration: InputDecoration(
+                          hintText: 'Selecione um bairro',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        items: bairros.map((bairro) {
+                          return DropdownMenuItem<String>(
+                            value: bairro,
+                            child: Text(bairro),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            selectedBairro = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(child: SizedBox(height: double.infinity)),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Rota Salva'),
+                            content: Text('A rota foi salva com sucesso!'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pushNamedAndRemoveUntil(
+                                    context,
+                                    '/home',
+                                    (route) => false,
+                                  );
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: Text('Salvar Rota'),
+                    style: ElevatedButton.styleFrom(
+                      textStyle: TextStyle(color: Colors.white, fontSize: 18.0),
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
